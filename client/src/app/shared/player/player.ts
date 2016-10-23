2d656e3f9cda7997f4ca1d763ca430d39a7beab2
Stephen A. Wilson <Stephen-356@hotmail.com>
2016-10-23 00:24:00 -0400

    add id to players

--- client/src/app/shared/player/player.ts
+++ client/src/app/shared/player/player.ts
@@ -1,8 +1,10 @@
 export class Player {
+  public id: number;
   public name: String;
   public score: number;
 
   constructor(player:any = {}) {
+    this.id = player.id || -1;
     this.name  = player.name || '';
     this.score = player.score || 0;
   }
